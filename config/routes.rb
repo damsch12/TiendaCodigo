0b0cc8653b05b4fa9b35254a088a48ad393f965d
Damian <damsch12@gmail.com>
2021-04-30 10:58:56 -0300

    Removed create, update and remove from products scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     end
   resources :questions
   devise_for :users
-  resources :products
+  resources :products, only: [:index, :show]
   resources :favourites, only: [:create, :destroy, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'
