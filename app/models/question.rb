fccf3651142e24073ce155c9e609251e9816eb07
Damian <damsch12@gmail.com>
2021-05-05 11:03:21 -0300

    Updated models with some validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
-    has_many :answers
+  has_many :answers
+  validates :question, :name, :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+  format: { with: VALID_EMAIL_REGEX, message: "Invalid email"  },
+            length: { minimum: 4, maximum: 254 }
 end
