81440db4db3264842719c96e75f0b0e931c4b33d
Damian <damsch12@gmail.com>
2021-05-11 10:08:33 -0300

    Filtered admin users from registered users statistic

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class StatsController < Admin::ApplicationController
     def index
-      @number_of_users = User.all.count
+      @number_of_users = User.where.not(role: 'Admin').count
       @unanswered_questions = Question.where(:answers.empty?).count
       @top_ten_fav = Product.order(favourite_counter: :desc).first(10)
       @top_ten_questions = Product.order(questions_counter: :desc).first(10)
