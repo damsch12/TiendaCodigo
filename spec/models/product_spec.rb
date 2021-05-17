c987156d8129bf7fad3bfe9950ccd1c903638690
Damian <damsch12@gmail.com>
2021-05-17 12:26:29 -0300

    Refactored specs to match standards, fix some login issues and avoid saving to DB

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe Product, type: :model do
     product = described_class.new(description: 'Product description', price: 200)
     expect(product.valid?).to eq(false)
     expect(product.errors[:name].include?("can't be blank")).to eq(true)
-    expect(product.errors[:name].include?("is too short (minimum is 10 characters)")).to eq(true)
+    expect(product.errors[:name].include?('is too short (minimum is 10 characters)')).to eq(true)
   end
-  
+
   it 'has a valid description' do
     product = described_class.new(name: 'Product name', price: 200)
     expect(product.valid?).to eq(false)
     expect(product.errors[:description].include?("can't be blank")).to eq(true)
-    expect(product.errors[:description].include?("is too short (minimum is 10 characters)")).to eq(true)
+    expect(product.errors[:description].include?('is too short (minimum is 10 characters)')).to eq(true)
   end
-  
+
   it 'has a valid price' do
     product = described_class.new(name: 'Product name', description: 'Product description', price: -1)
     expect(product.valid?).to eq(false)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
 include Warden::Test::Helpers
@@ -17,15 +17,27 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.backtrace_exclusion_patterns = [
-    /\/lib\d*\/ruby\//,
-    /bin\//,
+    %r{/lib\d*/ruby/},
+    %r{bin/},
     /gems/,
-    /org\/jruby\//,
-    /spec\/spec_helper\.rb/,
-    /lib\/rspec\/(core|expectations|matchers|mocks)/
+    %r{org/jruby/},
+    %r{spec/spec_helper\.rb},
+    %r{lib/rspec/(core|expectations|matchers|mocks)}
   ]
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
-#Capybara.default_driver = :selenium_chrome
+# Capybara.default_driver = :selenium_chrome
